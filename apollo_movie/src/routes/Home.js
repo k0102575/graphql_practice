e26cb67153c4b8e0c80f9378295042ae082180b9
k0102575 <k0102575@gmail.com>
2020-05-28 16:48:34 +0900

    apollo movie app 1.1

--- apollo_movie/src/routes/Home.js
+++ apollo_movie/src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
 
 const GET_MOVIES = gql`
   {
@@ -11,9 +12,54 @@ const GET_MOVIES = gql`
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 export default function Home() {
-  const { loading } = useQuery(GET_MOVIES);
+  const { loading, error, data } = useQuery(GET_MOVIES);
 
   console.log(loading);
-  return <div>Home</div>;
+  console.log(error);
+  console.log(data);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo Movie App</Title>
+        <Subtitle>Movie App built with React, Apollo and GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+    </Container>
+  );
 }
