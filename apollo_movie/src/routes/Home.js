9afb9c28bfcfd819198d86db2e6406ec0f692684
k0102575 <k0102575@gmail.com>
2020-05-28 16:25:06 +0900

    moviql clone

--- apollo_movie/src/routes/Home.js
+++ apollo_movie/src/routes/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 export default function Home() {
+  const { loading } = useQuery(GET_MOVIES);
+
+  console.log(loading);
   return <div>Home</div>;
 }
--- movieql/README.md
+++ movieql/README.md
@@ -1,6 +1,2 @@
-# GraphQL Practice - MOVIE QL
-
-[https://academy.nomadcoders.co/p/make-a-movie-api-with-graphql-and-nodejs-super-begginner](https://academy.nomadcoders.co/p/make-a-movie-api-with-graphql-and-nodejs-super-begginner)
-
-
-그래프 QL 설명 및 기초 연습
+# movieql
+Movie API with Graphql
