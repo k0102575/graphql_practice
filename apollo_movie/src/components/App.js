57fcf7e8a25fab682ad2129aabfefc3441514045
k0102575 <k0102575@gmail.com>
2020-05-28 16:04:08 +0900

    0

--- apollo_movie/src/components/App.js
+++ apollo_movie/src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <Route path="/" component={Home} exact></Route>
+      <Route path="/:id" component={Detail}></Route>
+    </Router>
+  );
 }
 
 export default App;
